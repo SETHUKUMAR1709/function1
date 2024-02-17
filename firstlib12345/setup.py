ea0253ca2c75da7036505a32488eafb8afead25f
SETHUKUMAR1709 <sethukumars774@gmail.com>
2024-02-17 15:59:30 +0530

    commit

--- firstlib12345/setup.py
+++ firstlib12345/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="firstlib",
-    version="0.0.10",
+    name="firstlib12345",
+    version="0.0.12",
     description="simple lib",
     package_dir={},
     packages=find_packages(),
@@ -17,5 +17,5 @@ setup(
     ],
     install_requires=[],
     extras_require={},
-    python_requires=">=3.11",
+    python_requires=">=3.10",
 )
