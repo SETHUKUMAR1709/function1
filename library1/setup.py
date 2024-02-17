1de11d45d33eead565bb27e98526e11f06440710
SETHUKUMAR1709 <sethukumars774@gmail.com>
2024-02-17 18:31:18 +0530

    commit

---
+++ library1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name = "sethukumar",
+    version= "0.0.1",
+    description= "my first library",
+    long_description= "This is my first library",
+    author="sethukumar moorthy",
+    packages= setuptools.find_packages(),
+    url="C:\Users\hp\Documents\GitHub\function1\library1",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+    install_requires=["pandas"],
+)
