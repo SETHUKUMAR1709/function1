39ad3ab3a58ff3549000a7d20b40cfa06cc7caab
SETHUKUMAR1709 <sethukumars774@gmail.com>
2024-02-17 16:51:54 +0530

    commit

--- firstlib12345/setup.py
+++ firstlib12345/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="firstlib12345",
-    version="0.0.14",
+    version="0.0.15",
     description="simple lib",
     package_dir={},
     packages=['src'],
